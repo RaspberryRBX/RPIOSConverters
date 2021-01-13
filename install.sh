3ae1e466a32ef5d001a5a08306e50eefcbfbbcd8
RaspberryRBX <61115942+RaspberryRBX@users.noreply.github.com>
2021-01-13 02:43:12 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+!/bin/bash
+sudo mkdir /usr/share/RPIThemes
+sudo cp MacOS /usr/bin
+sudo cp Windows /usr/bin
+sudo cp YT /usr/bin
+sudo chmod +x YT
+sudo chmod +x MacOS
+sudo chmod +x Windows
+echo Installation Completed Use It By ./Windows ./MacOS ./YT
+If It Say Permission Denied cd into /usr/bin sudo chmod +x Windows Or MacOS Or YT
